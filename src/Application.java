1418f2826d651ccc99cee4caeb39e17bd20159fb
Will Brown <kyrosewoodgang@gmail.com>
2021-03-16 17:19:52 -0400

    Update comments in test application

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,8 @@
 /**
- * 
- */
-
-/**
- * @author Will
- *
+ * Test application for the animal simulator.
+ * @author Will Brown
+ * @version 1.0
+ * Spring 2021
  */
 public class Application {
 
@@ -12,6 +10,7 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// This is highly incomplete and only here to make sure an exception is working peoperly.
 		Location test = new Location(1, -2);
 		System.out.println("Test");
 	}
